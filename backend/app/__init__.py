6d6c164d9b51bbc2b8a5749653100eb88c03729b
castellanosanalisis-cloud <castellanosanalisis@gmail.com>
2026-05-28 19:23:57 -0500

    Agregar __init__.py del backend

---
+++ backend/app/__init__.py
@@ -0,0 +1,5 @@
+"""NeuroPath - Infraestructura de Accesibilidad Cognitiva"""
+
+__version__ = "1.0.0"
+__author__ = "castellanosanalisis-cloud"
+__description__ = "Infraestructura EdTech ready-to-deploy para accesibilidad cognitiva"
